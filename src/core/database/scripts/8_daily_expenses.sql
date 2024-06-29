d648728e4c7c8a34319771e563b3266cef3a1f91
Susharnik Maksim <maksim.susharnik@acmecryptocorp.io>
2024-06-29 10:10:07 +0300

    init own branch

--- src/core/database/scripts/8_daily_expenses.sql
+++ src/core/database/scripts/8_daily_expenses.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS "daily_expenses" (
     expenses_id INT NOT NULL REFERENCES expenses(id),
     count INT DEFAULT NULL,
     price INT NOT NULL,
-    sum INT NOT NULL
+    sum INT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT now(),
+    updated TIMESTAMP NOT NULL DEFAULT now()
 );
